f0873e76aa1e57b323243a4221a131b29b96c52b
aburisarmon <kouhei00qant@gmail.com>
2020-06-19 17:23:04 +0900

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+function App(){
+  const [url, setUrl]  = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+  );
+  const [data, setData] = useState();
+  const [newData, setNewData] = useState();
+
+  useEffect(() => {
+      const fetch_date = async() => {
+        try{
+          const result = await axios(url);
+          const pokes = []
+          for (let i = 0; i < 10; i++){
+            const poke_data = await axios(result.data.results[i].url);
+            console.log(JSON.stringify(poke_data.sprites));
+            pokes.push(poke_data.sprites.front_default)
+          }
+          setData(
+            pokes
+          );
+        }catch{
+          console.log("error");
+        }
+      };
+      fetch_date();
+    }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+
+      </div>
+    </>
   );
 }
 
